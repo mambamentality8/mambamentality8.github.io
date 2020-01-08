e56c003476d4d91f13519f9fce710117d945b35d
mamba <mambamentality8g@gamil.com>
2020-01-08 23:50:01 +0800

    2020年01月08日

--- source/_posts/gitcommit.bat
+++ source/_posts/gitcommit.bat
@@ -1,6 +1,7 @@
 @echo off
-@title bat äº¤äº’æ‰§è¡Œgitå‘½ä»¤
+@title bat ½»»¥Ö´ĞĞgitÃüÁî
 D:
 cd E:\github\blog\mambamentality8.github.io\source\_posts
 git add .
-git commit -m %date:~0,4%å¹´%date:~5,2%æœˆ%date:~8,2%æ—¥
+git commit -m %date:~0,4%Äê%date:~5,2%ÔÂ%date:~8,2%ÈÕ
+git push origin hexo:hexo
